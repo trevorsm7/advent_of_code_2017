c1a290e5e59a5b910fd91051c56564a92ed5dcf5
Trevor Smith <trevorsm7@gmail.com>
2018-06-03 19:02:15 -0700

    Add solution for day 3 part 2

--- day3.rs
+++ day3.rs
@@ -1,4 +1,5 @@
 use std::env;
+use std::mem;
 
 fn part1(input: u32) -> u32 {
     // Handle trivial case
@@ -28,6 +29,82 @@ fn part1(input: u32) -> u32 {
     radius + tangent
 }
 
+fn part2(input: u32) -> u32 {
+    // Initialize the inner ring to a single 1
+    let mut inner = vec![1];
+    let mut outer = vec![];
+
+    // Iterate outward over concentric rings
+    let mut ring = 1;
+    loop {
+        // Compute num elements along each edge and around the ring
+        let edge_n = ring * 2;
+        let ring_n = edge_n * 4;
+
+        // NOTE these are invalid for the 0th ring, so we treat that as a special case
+        let inner_edge_n = (ring - 1) * 2;
+        let inner_ring_n = inner_edge_n * 4;
+
+        // Iterate over each index in the ring
+        for ring_i in 0..ring_n {
+            let edge_i = ring_i % edge_n;
+            let offset = (ring_i / edge_n) * inner_edge_n;
+
+            // ==== Get adjacent values in outer ring ====
+            // The previous index is always adjacent (if it exists!)
+            let mut val = *outer.last().unwrap_or(&0);
+
+            // The 1st index along each edge is adjacent to the 2nd to last index
+            if edge_i == 0 && ring_i > 1 {
+                val += outer[ring_i - 2];
+            };
+
+            // The last 2 indices in the ring are adjacent to the 1st
+            if ring_n - ring_i <= 2 {
+                val += outer[0];
+            };
+
+            // ==== Get adjacent values in inner ring ====
+            if ring == 1 {
+                // Special case for 0th ring
+                val += inner[0];
+            } else {
+                if edge_i == edge_n - 1 {
+                    // This is a corner, so the corner of the inner ring is adjacent
+                    val += inner[offset + inner_edge_n - 1];
+                } else {
+                    // Get the adjacent square first
+                    let adjacent = offset + edge_i + inner_ring_n - 1;
+                    val += inner[adjacent % inner_ring_n];
+
+                    // Get the backward diagonal
+                    if edge_i > 0 {
+                        val += inner[(adjacent - 1) % inner_ring_n];
+                    }
+
+                    // Get the forwards diagonal
+                    if edge_i < edge_n - 2 {
+                        val += inner[(adjacent + 1) % inner_ring_n];
+                    }
+                };
+            };
+
+            // Return the first value larger than input
+            if val > input {
+                return val;
+            };
+
+            // Cache the value in the outer ring
+            outer.push(val);
+        };
+
+        // Swap outer to inner for next ring (swaps refs, does not move elements)
+        mem::swap(&mut inner, &mut outer);
+        outer.clear();
+        ring += 1;
+    };
+}
+
 fn main() -> Result<(), std::io::Error> {
     // Expect input to be passed as first arg
     let input = env::args()
@@ -38,6 +115,7 @@ fn main() -> Result<(), std::io::Error> {
         .expect("expected argument to be a number");
 
     println!("Part 1: {}", part1(input));
+    println!("Part 2: {}", part2(input));
 
     Ok(())
 }
