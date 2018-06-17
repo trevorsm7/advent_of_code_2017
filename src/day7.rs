d714fde95137d97a5a836e93ecae98daa9a7b700
Trevor Smith <trevorsm7@gmail.com>
2018-06-17 00:00:30 -0700

    Refactor before adding day part 2

--- src/day7.rs
+++ src/day7.rs
@@ -1,19 +1,20 @@
 use std::fs;
 use std::env;
 use std::io;
-use std::collections::BTreeMap;
+use std::collections::HashMap;
 
+#[derive(Clone)]
 struct Tower {
     //name: String,
-    //weight: u32,
+    weight: u32,
     children: Vec<String>,
 }
 
 impl Tower {
-    pub fn with_name(_name: &str, _weight: u32) -> Self {
+    pub fn with_name(_name: &str, weight: u32) -> Self {
         Self {
             //name: name.to_string(),
-            //weight: weight,
+            weight: weight,
             children: Vec::new(),
         }
     }
@@ -23,51 +24,71 @@ impl Tower {
     }
 }
 
-fn part1(input: &str) -> String {
-    let mut towers = BTreeMap::new();
+type TowerMap = HashMap<String, Tower>;
 
-    // Parse the input line-by-line
-    for line in input.lines() {
-        // Split line with whitespace and punctuation
-        let mut iter = line
-            .split_terminator(|c: char| c.is_whitespace() || c.is_ascii_punctuation())
-            .filter(|s| !s.is_empty());
+trait HashMapExt {
+    fn from_input(input: &str) -> TowerMap;
+    fn find_parent(&self) -> String;
+}
 
-        // Read name and weight, create new Tower
-        let name = iter.next().unwrap();
-        let weight = iter.next().unwrap().parse().unwrap();
-        let mut tower = Tower::with_name(name, weight);
+impl HashMapExt for TowerMap {
+    fn from_input(input: &str) -> TowerMap {
+        let mut tower_map = Self::new();
 
-        // If there are any children, save their names
-        for child in iter {
-            tower.push_child(child);
+        // Parse the input line-by-line
+        for line in input.lines() {
+            // Split line with whitespace and punctuation
+            let mut iter = line
+                .split_terminator(|c: char| c.is_whitespace() || c.is_ascii_punctuation())
+                .filter(|s| !s.is_empty());
+
+            // Read name and weight, create new Tower
+            let name = iter.next().unwrap();
+            let weight = iter.next().unwrap().parse().unwrap();
+            let mut tower = Tower::with_name(name, weight);
+
+            // If there are any children, save their names
+            for child in iter {
+                tower.push_child(child);
+            }
+
+            // Store the tower by name in our map
+            tower_map.insert(name.to_string(), tower);
         }
 
-        // Store the tower by name in our map
-        towers.insert(name.to_string(), tower);
+        tower_map
     }
 
-    let mut all_children = Vec::new();
+    fn find_parent(&self) -> String {
+        let mut towers = self.clone();
+        let mut all_children = Vec::new();
 
-    // Record all names referenced as children
-    for tower in towers.values() {
-        for name in &tower.children {
-            all_children.push(name.to_string());
+        // Record all names referenced as children
+        for tower in towers.values() {
+            for name in &tower.children {
+                all_children.push(name.to_string());
+            }
         }
-    }
 
-    // Remove all children from the list
-    for name in &all_children {
-        towers.remove(name).unwrap();
-    }
+        // Remove all children from the set
+        for name in &all_children {
+            towers.remove(name).unwrap();
+        }
 
-    // Return the last key in the list
-    assert_eq!(towers.len(), 1);
-    towers.keys().next().unwrap().to_string()
+        // Return the last key in the set
+        assert_eq!(towers.len(), 1);
+        towers.keys().next().unwrap().to_string()
+    }
+}
+
+fn dewit(input: &str) -> (String, u32) {
+    let towers = TowerMap::from_input(input);
+    let parent = towers.find_parent();
+    (parent, 50)
 }
 
 #[test]
-fn test_day7_part1() {
+fn test_day7() {
     let input =
 "pbga (66)
 xhth (57)
@@ -82,7 +103,7 @@ jptl (61)
 ugml (68) -> gyxo, ebii, jptl
 gyxo (61)
 cntj (57)";
-    assert_eq!(part1(&input), "tknk");
+    assert_eq!(dewit(&input), ("tknk".to_string(), 60));
 }
 
 pub fn day7(args: &mut env::Args) -> Result<(), io::Error> {
@@ -92,8 +113,8 @@ pub fn day7(args: &mut env::Args) -> Result<(), io::Error> {
         fs::read_to_string(name)?
     };
 
-    println!("Part 1: {}", part1(&input));
-    //println!("Part 2: {}", part2(&input));
+    let (part1, part2) = dewit(&input);
+    println!("Part 1: {}\nPart 2: {}", part1, part2);
 
     Ok(())
 }
