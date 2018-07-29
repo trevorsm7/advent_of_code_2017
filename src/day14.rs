69804ced7072e7fe83ee705cb6b88c31b021079e
Trevor Smith <trevorsm7@gmail.com>
2018-07-28 21:10:37 -0700

    Add solution for day 14 part 2

--- src/day14.rs
+++ src/day14.rs
@@ -1,41 +1,98 @@
 use day10;
 use std::env;
 use std::io;
+use std::collections::BTreeSet;
 
-fn part1(input: &str) -> usize {
-    let mut used = 0;
+fn dewit(input: &str) -> (usize, usize) {
+    let mut used_count = 0;
+    let mut used_set = BTreeSet::new();
 
-    for row in 0..128 {
+    // Number of rows must be a power of 2 up to 128
+    const N_ROWS : u32 = 128;
+    assert_eq!(N_ROWS & (N_ROWS - 1), 0);
+
+    for row in 0..N_ROWS {
         // Compute the knot hash for this row
-        // NOTE knot_hash could just return a 128 bit integer, but I'm sticking to the prompt
         let row_input = format!("{}-{}", input, row);
         let hash = day10::knot_hash(&row_input);
 
-        for c in hash.chars() {
-            // Use a lookup table rather than count the bits each time
-            used += match c {
-                '1' | '2' | '4' | '8' => 1,
-                '3' | '5' | '6' | '9' | 'a' | 'c' => 2,
-                '7' | 'b' | 'd' | 'e' => 3,
-                'f' => 4,
-                _ => 0,
-            };
+        // We can easily parse this back into a 128-bit integer
+        let mut hash_bits = u128::from_str_radix(&hash, 16).expect("Unable to parse hash");
+
+        // Loop over each set bit from the rhs
+        while hash_bits > 0 {
+            used_count += 1;
+
+            // Get the index of the right-most bit by counting zeros
+            let col = hash_bits.trailing_zeros();
+
+            // Use bit-twiddling trick to clear the right-most bit
+            // XX1000 - 1 = XX0111
+            // XX1000 & XX0111 = XX0000
+            hash_bits = hash_bits & (hash_bits - 1);
+
+            // Insert the index (from rhs!) of this bit into our set
+            used_set.insert(row * N_ROWS + col);
         }
     }
 
-    used
+    let mut region_count = 0;
+    let mut to_visit = Vec::new();
+
+    // While there are set bits
+    while !used_set.is_empty() {
+        region_count += 1;
+
+        // Visit one of the set bits
+        to_visit.push(*used_set.iter().next().unwrap());
+
+        // While there are bits to visit
+        while !to_visit.is_empty() {
+            // Pop an index from the set (depth-first)
+            let index = to_visit.pop().unwrap();
+
+            // If the bit at the index is set...
+            if used_set.remove(&index) {
+                let row = index >> N_ROWS.trailing_zeros();
+                let col = index & (N_ROWS - 1);
+
+                // Visit the bit above
+                if row > 0 {
+                    to_visit.push(index - N_ROWS);
+                }
+
+                // Visit the bit below
+                if row < N_ROWS - 1 {
+                    to_visit.push(index + N_ROWS);
+                }
+
+                // Visit the bit to the left
+                if col > 0 {
+                    to_visit.push(index - 1);
+                }
+
+                // Visit the bit to the right
+                if col < N_ROWS - 1 {
+                    to_visit.push(index + 1);
+                }
+            }
+        }
+    }
+
+    (used_count, region_count)
 }
 
 #[test]
-fn test_day14_part1() {
-    assert_eq!(part1("flqrgnkx"), 8108);
+fn test_day14() {
+    assert_eq!(dewit("flqrgnkx"), (8108, 1242));
 }
 
 pub fn day14(args: &mut env::Args) -> Result<(), io::Error> {
     // Read from command line
     let input = args.next().unwrap_or("hxtvlmkl".to_string());
 
-    println!("Knot hash: {}", part1(&input));
+    let (part1, part2) = dewit(&input);
+    println!("Part 1: {}\nPart 2: {}", part1, part2);
 
     Ok(())
 }
