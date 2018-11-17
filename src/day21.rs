e4111a749208cdafd47b9f44d5d3610ed384e66e
Trevor Smith <trevorsm7@gmail.com>
2018-11-17 00:05:47 -0800

    Add day21 functions and unit tests for 3x3 blocks; part 1 finished

--- src/day21.rs
+++ src/day21.rs
@@ -13,13 +13,13 @@ pub fn day21(args: &mut env::Args) -> Result<(), Error> {
         fs::read_to_string(name)?
     };
 
-    println!("Part 1: {}", part1(&input));
+    println!("Part 1: {}", part1(&input, 5));
     //println!("Part 2: {}", part2(&input));
 
     Ok(())
 }
 
-fn part1(input: &str) -> u32 {
+fn part1(input: &str, steps: usize) -> u8 {
     let (pat2x2, pat3x3) = read_patterns(input);
 
     //.#.
@@ -31,19 +31,24 @@ fn part1(input: &str) -> u32 {
         1, 1, 1];
     let mut back = Vec::new();
 
-    for i in 0..5 {
+    for i in 0..steps {
         if front.len() % 4 == 0 {
             enhance_2x2(&mut front, &mut back, &pat2x2);
         }
         else {
-            // TODO 3x3
+            enhance_3x3(&mut front, &mut back, &pat3x3);
         }
-
-        // Swap front and back buffers
-        mem::swap(&mut front, &mut back);
     }
 
-    0
+    front.iter().sum()
+}
+
+#[test]
+fn test_day21_part1() {
+    let input = "\
+        ../.# => ##./#../...\n\
+        .#./..#/### => #..#/..../..../#..#";
+    assert_eq!(part1(&input, 2), 12);
 }
 
 fn read_patterns(input: &str) -> (Vec<Pattern2x2>, Vec<Pattern3x3>) {
@@ -131,7 +136,8 @@ fn test_day21_read_patterns() {
          0, 1]));
 }
 
-fn enhance_2x2(front: &Vec<u8>, back: &mut Vec<u8>, patterns: &[Pattern2x2]) {
+fn enhance_2x2<'a>(front: &'a mut Vec<u8>, back: &'a mut Vec<u8>, patterns: &[Pattern2x2]) {
+    assert_eq!(front.len() % 4, 0);
     let n = front.len() / 4;
     back.resize(9 * n, 0);
 
@@ -142,12 +148,15 @@ fn enhance_2x2(front: &Vec<u8>, back: &mut Vec<u8>, patterns: &[Pattern2x2]) {
             match_pattern_2x2(&front[f1..f2], patterns)
                 .expect(&format!("Unable to match pattern {:?}", &front[f1..f2])));
     }
+
+    // Swap front and back buffers
+    mem::swap(front, back);
 }
 
 #[test]
 fn test_day21_enhance_2x2() {
     let (patterns, _) = read_patterns("../.# => ##./#../...");
-    let front = vec![
+    let mut front = vec![
         1, 0,
         0, 0,
         0, 1,
@@ -157,14 +166,74 @@ fn test_day21_enhance_2x2() {
         0, 0,
         0, 1];
     let mut back = Vec::new();
-    enhance_2x2(&front, &mut back, &patterns);
+    enhance_2x2(&mut front, &mut back, &patterns);
     for i in 0..4 {
         let (a, b) = (i * 9, (i + 1) * 9);
         assert_eq!(
-            &back[a..b],
+            &front[a..b],
             &[1, 1, 0,
               1, 0, 0,
               0, 0, 0]);
+    }
+}
+
+fn enhance_3x3<'a>(front: &'a mut Vec<u8>, back: &'a mut Vec<u8>, patterns: &[Pattern3x3]) {
+    assert_eq!(front.len() % 9, 0);
+    let n = front.len() / 9;
+    back.resize(16 * n, 0);
+
+    let n2 = (n as f64).sqrt() as usize;
+    for j in 0..n2 {
+        for i in 0..n2 {
+            let (f1, f2) = ((j * n2 + i) * 9, (j * n2 + i + 1) * 9);
+            let (b1, b2) = ((j * 2 * n2 + i) * 8, (j * 2 * n2 + i + 1) * 8);
+            let (b3, b4) = (((j * 2 + 1) * n2 + i) * 8, ((j * 2 + 1) * n2 + i + 1) * 8);
+            let (ref stamp1, ref stamp2) = match_pattern_3x3(&front[f1..f2], patterns)
+                .expect(&format!("Unable to match pattern {:?}", &front[f1..f2]));
+            back[b1..b2].copy_from_slice(stamp1);
+            back[b3..b4].copy_from_slice(stamp2);
+        }
+    }
+
+    // Swap front and back buffers
+    mem::swap(front, back);
+}
+
+#[test]
+fn test_day21_enhance_3x3() {
+    let (_, patterns) = read_patterns(".#./..#/### => #..#/..../..../#..#");
+    let mut front = vec![
+        0, 1, 0,
+        0, 0, 1,
+        1, 1, 1,
+        1, 0, 0,
+        1, 0, 1,
+        1, 1, 0,
+        1, 1, 1,
+        1, 0, 0,
+        0, 1, 0,
+        0, 1, 1,
+        1, 0, 1,
+        0, 0, 1];
+    let mut back = Vec::new();
+    enhance_3x3(&mut front, &mut back, &patterns);
+    for j in 0..2 {
+        for i in 0..2 {
+            let (a, b) = (((j * 4) + i) * 8, ((j * 4) + i + 1) * 8);
+            assert_eq!(
+                &front[a..b],
+                &[1, 0,
+                  0, 0,
+                  0, 1,
+                  0, 0]);
+            let (a, b) = (((j * 4) + i + 2) * 8, ((j * 4) + i + 3) * 8);
+            assert_eq!(
+                &front[a..b],
+                &[0, 0,
+                  1, 0,
+                  0, 0,
+                  0, 1]);
+        }
     }
 }
 
@@ -201,6 +270,48 @@ fn test_day21_match_pattern_2x2() {
         &[1, 1,
           0, 0];
     assert_eq!(match_pattern_2x2(input, &patterns), None);
+}
+
+fn match_pattern_3x3<'a>(input: &[u8], patterns: &'a [Pattern3x3]) -> Option<(&'a [u8], &'a [u8])> {
+    // Pre-compute all 8 transforms of the input
+    let transforms = transform_3x3(input);
+
+    // Try to match each transform with each pattern
+    for (ref lr, ref hr1, ref hr2) in patterns {
+        for &source in &transforms {
+            if *lr == source {
+                return Some((hr1, hr2));
+            }
+        }
+    }
+
+    None
+}
+
+#[test]
+fn test_day21_match_pattern_3x3() {
+    let (_, patterns) = read_patterns(".#./..#/### => #..#/..../..../#..#");
+
+    let input =
+        [1, 0, 0,
+         1, 0, 1,
+         1, 1, 0];
+    let output =
+        [1, 0,
+         0, 0,
+         0, 1,
+         0, 0,
+         0, 0,
+         1, 0,
+         0, 0,
+         0, 1];
+    assert_eq!(match_pattern_3x3(&input, &patterns).unwrap(), (&output[..8], &output[8..]));
+
+    let input =
+        &[1, 1, 0,
+          0, 1, 0,
+          1, 1, 0];
+    assert_eq!(match_pattern_3x3(input, &patterns), None);
 }
 
 fn transform_2x2(input: &[u8]) -> Vec<[u8; 4]> {
@@ -271,4 +382,72 @@ fn test_day21_transform_2x2() {
              1, 0],
             [0, 0,
              1, 1]]);
+}
+
+fn transform_3x3(input: &[u8]) -> Vec<[u8; 9]> {
+    let mut transforms = Vec::with_capacity(8);
+    transforms.resize(8, [0; 9]);
+    transforms[0].copy_from_slice(input);
+
+    for i in 1..4 {
+        // Rotate source 90 degrees into target
+        transforms[i][2] = transforms[i - 1][0];
+        transforms[i][5] = transforms[i - 1][1];
+        transforms[i][8] = transforms[i - 1][2];
+        transforms[i][1] = transforms[i - 1][3];
+        transforms[i][4] = transforms[i - 1][4];
+        transforms[i][7] = transforms[i - 1][5];
+        transforms[i][0] = transforms[i - 1][6];
+        transforms[i][3] = transforms[i - 1][7];
+        transforms[i][6] = transforms[i - 1][8];
+    }
+
+    for i in 0..4 {
+        // Flip source into target
+        transforms[i + 4][2] = transforms[i][0];
+        transforms[i + 4][1] = transforms[i][1];
+        transforms[i + 4][0] = transforms[i][2];
+        transforms[i + 4][5] = transforms[i][3];
+        transforms[i + 4][4] = transforms[i][4];
+        transforms[i + 4][3] = transforms[i][5];
+        transforms[i + 4][8] = transforms[i][6];
+        transforms[i + 4][7] = transforms[i][7];
+        transforms[i + 4][6] = transforms[i][8];
+    }
+
+    transforms
+}
+
+#[test]
+fn test_day21_transform_3x3() {
+    assert_eq!(
+        transform_3x3(&
+            [1, 1, 0,
+             0, 1, 0,
+             0, 0, 0]),
+        vec![
+            [1, 1, 0,
+             0, 1, 0,
+             0, 0, 0],
+            [0, 0, 1,
+             0, 1, 1,
+             0, 0, 0],
+            [0, 0, 0,
+             0, 1, 0,
+             0, 1, 1],
+            [0, 0, 0,
+             1, 1, 0,
+             1, 0, 0],
+            [0, 1, 1,
+             0, 1, 0,
+             0, 0, 0],
+            [1, 0, 0,
+             1, 1, 0,
+             0, 0, 0],
+            [0, 0, 0,
+             0, 1, 0,
+             1, 1, 0],
+            [0, 0, 0,
+             0, 1, 1,
+             0, 0, 1]]);
 }
