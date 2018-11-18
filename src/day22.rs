3fa313a55625b47cef5f41c78dabcb1e5dfebe3b
Trevor Smith <trevorsm7@gmail.com>
2018-11-18 15:14:24 -0800

    Add day22 part1

--- src/day22.rs
+++ src/day22.rs
@@ -2,6 +2,12 @@ use std::{fs, env, cmp};
 use std::io::Error;
 use std::collections::HashSet;
 
+type Point = (i32, i32);
+type Grid = HashSet<Point>;
+
+// Up, right, down, left; +1 clockwise, -1 counter-clockwise
+const HEADINGS: [Point; 4] = [(0, -1), (1, 0), (0, 1), (-1, 0)];
+
 pub fn day22(args: &mut env::Args) -> Result<(), Error> {
     // Read from file in first arg or default to input.txt
     let input = {
@@ -9,26 +15,57 @@ pub fn day22(args: &mut env::Args) -> Result<(), Error> {
         fs::read_to_string(name)?
     };
 
-    println!("Part 1: {}", part1(&input));
+    println!("Part 1: {}", part1(&input, 10000));
     //println!("Part 2: {}", part2(&input));
 
     Ok(())
 }
 
-fn part1(input: &str) -> u32 {
+fn part1(input: &str, bursts: usize) -> usize {
     let mut grid = Grid::from_string(input);
-    println!("{}", grid.to_string());
 
-    0
+    let mut carrier = (0, 0);
+    let mut heading = 0;
+
+    let mut infections = 0;
+    for _ in 0..bursts {
+        // If node is infected
+        if grid.remove(&carrier) {
+            // Turn right, clean node
+            heading = (heading + 1) % 4;
+        }
+        else {
+            // Turn left, infect node
+            heading = (heading + 3) % 4;
+            grid.insert(carrier);
+            infections += 1;
+        }
+
+        // Advance carrier one step
+        carrier.0 += HEADINGS[heading].0;
+        carrier.1 += HEADINGS[heading].1;
+    }
+
+    infections
 }
 
-type Point = (i32, i32);
-
-struct Grid {
-    grid: HashSet<Point>,
+#[test]
+fn test_day22_part1() {
+    let input = "\
+        ..#\n\
+        #..\n\
+        ...";
+    assert_eq!(part1(&input, 7), 5);
+    assert_eq!(part1(&input, 70), 41);
+    assert_eq!(part1(&input, 10000), 5587);
 }
 
-impl Grid {
+trait GridExt {
+    fn from_string(input: &str) -> Self;
+    fn to_string(&self) -> String;
+}
+
+impl GridExt for Grid {
     fn from_string(input: &str) -> Self {
         let mut grid = HashSet::new();
 
@@ -50,8 +87,8 @@ impl Grid {
             }
             y += 1;
         }
-        
-        Self { grid }
+
+        grid
     }
 
     fn to_string(&self) -> String {
@@ -60,7 +97,7 @@ impl Grid {
         let mut max_x = 0;
         let mut min_y = 0;
         let mut max_y = 0;
-        for &(x, y) in &self.grid {
+        for &(x, y) in self {
             min_x = cmp::min(min_x, x);
             max_x = cmp::max(max_x, x);
             min_y = cmp::min(min_y, y);
@@ -73,7 +110,7 @@ impl Grid {
         let mut rows = vec![vec!['.'; w as usize]; h as usize];
 
         // Plot each point in the grid
-        for &(x, y) in &self.grid {
+        for &(x, y) in self {
             let row = (y + (h - 1) / 2) as usize;
             let col = (x + (w - 1) / 2) as usize;
             rows[row][col] = '#';
